1ddbb4a18f7e77e34be5e98ddf40531b4bbfd796
moonorange <monoma632@gmail.com>
2020-10-24 23:20:34 +0900

    add: some sums

---
+++ first_problems/some_sums.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int     rec_sum_digits(int num)
+{
+    int sum;
+
+    if (num < 10)
+        return num;
+    return (num % 10 + rec_sum_digits(num / 10));
+}
+
+int     main(void)
+{
+    int n;
+    int a;
+    int b;
+    int i = 1;
+    int sum;
+    int sum_digits;
+
+    scanf("%d %d %d", &n, &a, &b);
+
+    for (; i <= n; i++)
+    {
+        sum_digits = rec_sum_digits(i);
+        if (a <= sum_digits && sum_digits <= b)
+        {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+}
