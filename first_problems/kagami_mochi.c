959a9a9d51328cf3393d9e41ceebbaf03f1f6841
moonorange <monoma632@gmail.com>
2020-11-01 17:29:20 +0900

    create: kagami mochi file

---
+++ first_problems/kagami_mochi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+    int n;
+    int i;
+    int array[n];
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+
+}
