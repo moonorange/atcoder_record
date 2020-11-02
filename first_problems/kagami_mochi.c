d39939588c4a39f0e0dd6acac2a260b253a7edd7
moonorange <monoma632@gmail.com>
2020-11-02 21:21:06 +0900

    add: kagamimochi

--- first_problems/kagami_mochi.c
+++ first_problems/kagami_mochi.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
+int compare(const void *a, const void *b){
+    return *(int*)b - *(int*)a;
+}
 
 int main(void)
 {
     int n;
     int i;
     int array[n];
+    int layer = 1;
+    int buffer;
 
     scanf("%d", &n);
     for (i = 0; i < n; i++)
     {
         scanf("%d", &array[i]);
     }
-
-
+    qsort(array, n, 4, compare);
+    buffer = array[0];
+    for (i = 0; i < n; i++)
+    {
+        if (array[i] != buffer)
+            layer++;
+    }
+    printf("%d\n", layer);
 }
