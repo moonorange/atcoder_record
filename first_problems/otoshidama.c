64614a1c05c57c48c358345503ab2f86286f538e
moonorange <monoma632@gmail.com>
2020-11-02 23:58:47 +0900

    add: otoshidama.c

---
+++ first_problems/otoshidama.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n,y;
+    int i,j,k;
+
+    scanf("%d %d", &n, &y);
+
+    i = 0;
+    while (i <= n)
+    {
+        j = 0;
+        while (j <= n - i)
+        {
+            k = 0;
+            while (k <= n - (i + j))
+            {
+                if (10000*i + 5000*j + 1000*k == y)
+                {
+                    printf("%d %d %d\n", i, j, k);
+                    return (0);
+                }
+                k++;
+            }
+            j++;
+        }
+        i++;
+    }
+    printf("-1 -1 -1\n");
+}
